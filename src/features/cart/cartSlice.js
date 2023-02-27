85fd2865d79b15d170d3fa430e0af8ae39ab7b86
Anthony Martin <anthonymartin20@gmail.com>
2023-02-27 16:37:02 +0100

    Remove console.log

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -63,7 +63,6 @@ const cartSlice = createSlice({
             state.isLoading = true;
         },
         [getCartItems.fulfilled]:(state, action) => {
-            console.log(action);
             state.isLoading = false;
             state.cartItems = action.payload;
         },
